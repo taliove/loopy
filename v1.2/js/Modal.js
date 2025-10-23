5dcac117349459b241967884ea2ef4ba259ca117
taliove <taliove@vip.qq.com>
2025-10-23 16:28:45 +0800

    version: 1.2

---
+++ v1.2/js/Modal.js
@@ -0,0 +1,280 @@
+/***********************
+
+Use the same PAGE UI thing
+
+************************/
+
+function Modal(loopy){
+
+	var self = this;
+	self.loopy = loopy;
+	PageUI.call(self, document.getElementById("modal_page"));
+
+	// Is showing?
+	self.isShowing = false;
+
+	// show/hide
+	self.show = function(){
+		document.getElementById("modal_container").setAttribute("show","yes");
+		self.isShowing = true;
+	};
+	self.hide = function(){
+		document.getElementById("modal_container").setAttribute("show","no");
+		if(self.currentPage.onhide) self.currentPage.onhide();
+		self.isShowing = false;
+	};
+
+	// Close button
+	document.getElementById("modal_bg").onclick = self.hide;
+	document.getElementById("modal_close").onclick = self.hide;
+
+	// Show... what page?
+	subscribe("modal", function(pageName){
+
+		self.show();
+		var page = self.showPage(pageName);
+
+		// Do something
+		if(page.onshow) page.onshow();
+
+		// Dimensions
+		var dom = document.getElementById("modal");
+		dom.style.width = self.currentPage.width+"px";
+		dom.style.height = self.currentPage.height+"px";
+
+	});
+
+	///////////////////
+	// PAGES! /////////
+	///////////////////
+
+	// Examples
+	(function(){
+		var page = new Page();
+		page.width = 670;
+		page.height = 570;
+		var iframe = page.addComponent(new ModalIframe({
+			page: page,
+			src: "pages/examples/",
+			width: 640,
+			height: 520
+		}));
+		iframe.dom.style.background = "#f7f7f7";
+		self.addPage("examples", page);
+	})();
+
+	// How To
+	(function(){
+		var page = new Page();
+		page.width = 530;
+		page.height = 430;
+		page.addComponent(new ModalIframe({
+			page: page,
+			src: "pages/howto.html",
+			width: 500,
+			height: 350
+		}));
+
+		var label = document.createElement("div");
+		label.style.fontSize = "18px";
+		label.style.marginTop = "6px";
+		label.style.color = "#777";
+		label.innerHTML = "需要模拟的灵感？查看<span style='text-decoration:underline; cursor:pointer' onclick='publish(\"modal\",[\"examples\"])'>示例！</span>";
+		page.dom.appendChild(label);
+
+		self.addPage("howto", page);
+
+	})();
+
+	// Credits
+	(function(){
+		var page = new Page();
+		page.width = 690;
+		page.height = 550;
+		page.addComponent(new ModalIframe({
+			page: page,
+			src: "pages/credits/",
+			width: 660,
+			height: 500
+		}))
+		self.addPage("credits", page);
+	})();
+
+	// Save as link
+	(function(){
+		var page = new Page();
+		page.width = 500;
+		page.height = 155;
+		page.addComponent(new ComponentHTML({
+			html: "复制您的链接:"
+		}));
+		var output = page.addComponent(new ComponentOutput({}));
+
+		var label = document.createElement("div");
+		label.style.textAlign = "right";
+		label.style.fontSize = "15px";
+		label.style.marginTop = "6px";
+		label.style.color = "#888";
+		label.innerHTML = "(这是一个很长的 URL，您可能需要使用短链接服务，如 <a target='_blank' href='https://bitly.com/'>bit.ly</a>)";
+		page.dom.appendChild(label);
+
+		// chars left...
+		var chars = document.createElement("div");
+		chars.style.textAlign = "right";
+		chars.style.fontSize = "15px";
+		chars.style.marginTop = "3px";
+		chars.style.color = "#888";
+		chars.innerHTML = "X / 2048 字符";
+		page.dom.appendChild(chars);
+
+		page.onshow = function(){
+
+			// Copy-able link
+			var link = loopy.saveToURL();
+			output.output(link);
+			output.dom.select();
+
+			// Chars left
+			var html = link.length+" / 2048 字符";
+			if(link.length>2048){
+				html += " - 对于大多数浏览器可能太长";
+			}
+			chars.innerHTML = html;
+			chars.style.fontWeight = (link.length>2048) ? "bold" : "100";
+			chars.style.fontSize = (link.length>2048) ? "14px" : "15px";
+
+		};
+
+		// or, tweet it
+		self.addPage("save_link", page);
+	})();
+
+	// Embed
+	(function(){
+		var page = new Page();
+		page.width = 700;
+		page.height = 500;
+
+		// ON UPDATE DIMENSIONS
+		var iframeSRC;
+		var _onUpdate = function(){
+			var embedCode = '<iframe width="'+width.getValue()+'" height="'+height.getValue()+'" frameborder="0" src="'+iframeSRC+'"></iframe>';
+			output.output(embedCode);
+		};
+
+		// THE SHTUFF
+		var sidebar = document.createElement("div");
+		sidebar.style.width = "150px";
+		sidebar.style.height = "440px";
+		sidebar.style.float = "left";
+		page.dom.appendChild(sidebar);
+
+		// Label
+		var label = document.createElement("div");
+		label.innerHTML = "<br>预览 &rarr;<br><br>";
+		sidebar.appendChild(label);
+
+		// Label 2
+		var label = document.createElement("div");
+		label.style.fontSize = "15px";
+		label.innerHTML = "您希望嵌入的尺寸是多少？";
+		sidebar.appendChild(label);
+
+		// Size!
+		var width = _createNumberInput(_onUpdate);
+		sidebar.appendChild(width.dom);
+		var label = document.createElement("div");
+		label.style.display = "inline-block";
+		label.style.fontSize = "15px";
+		label.innerHTML = "&nbsp;×&nbsp;";
+		sidebar.appendChild(label);
+		var height = _createNumberInput(_onUpdate);
+		sidebar.appendChild(height.dom);
+
+		// Label 3
+		var label = document.createElement("div");
+		label.style.fontSize = "15px";
+		label.innerHTML = "<br><br>将此代码复制到您网站的 HTML 中:";
+		sidebar.appendChild(label);
+
+		// Output!
+		var output = new ComponentOutput({});
+		output.dom.style.fontSize = "12px";
+		sidebar.appendChild(output.dom);
+
+		// Label 3
+		var label = document.createElement("div");
+		label.style.fontSize = "15px";
+		label.style.textAlign = "right";
+		label.innerHTML = "<br><br>(注意：REMIX 按钮允许其他人重新混合您的模型！别担心，这只是一个副本，不会影响原始模型。)";
+		sidebar.appendChild(label);
+
+		// IFRAME
+		var iframe = page.addComponent(new ModalIframe({
+			page: page,
+			manual: true,
+			src: "",
+			width: 500,
+			height: 440
+		})).dom;
+		iframe.style.float = "right";
+		page.onshow = function(){
+
+			// Default dimensions
+			width.setValue(500);
+			height.setValue(440);
+
+			// The iframe!
+			iframeSRC = loopy.saveToURL(true);
+			iframe.src = iframeSRC;
+
+			// Select to copy-paste
+			_onUpdate();
+			output.dom.select();
+
+		};
+		page.onhide = function(){
+			iframe.removeAttribute("src");
+		};
+		self.addPage("embed", page);
+
+
+	})();
+
+	// GIF
+	(function(){
+		var page = new Page();
+		page.width = 530;
+		page.height = 400;
+		page.addComponent(new ModalIframe({
+			page: page,
+			src: "pages/gif.html",
+			width: 500,
+			height: 350
+		}))
+		self.addPage("save_gif", page);
+	})();
+
+}
+
+function ModalIframe(config){
+
+	var self = this;
+
+	// IFRAME
+	var iframe = document.createElement("iframe");
+	self.dom = iframe;
+	iframe.width = config.width;
+	iframe.height = config.height;
+
+	// Show & Hide
+	if(!config.manual){
+		config.page.onshow = function(){
+			iframe.src = config.src;
+		};
+		config.page.onhide = function(){
+			iframe.removeAttribute("src");
+		};
+	}
+
+}
---
+++ v1.2/js/Sidebar.js
@@ -0,0 +1,510 @@
+/**********************************
+
+SIDEBAR CODE
+
+**********************************/
+
+function Sidebar(loopy){
+
+	var self = this;
+	PageUI.call(self, document.getElementById("sidebar"));
+
+	// Edit
+	self.edit = function(object){
+		self.showPage(object._CLASS_);
+		self.currentPage.edit(object);
+	};
+
+	// Go back to main when the thing you're editing is killed
+	subscribe("kill",function(object){
+		if(self.currentPage.target==object){
+			self.showPage("Edit");
+		}
+	});
+
+	////////////////////////////////////////////////////////////////////////////////////////////
+	// ACTUAL PAGES ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////
+
+	// Node!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "返回顶部",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("label", new ComponentInput({
+			label: "<br><br>名称:"
+			//label: "Name:"
+		}));
+		page.addComponent("shape", new ComponentSlider({
+			bg: "shape",
+			label: "形状:",
+			options: ["circle", "rectangle"],
+			oninput: function(value){
+				Node.defaultShape = value;
+			}
+		}));
+		page.addComponent("hue", new ComponentSlider({
+			bg: "color",
+			label: "颜色:",
+			options: [0,1,2,3,4,5],
+			oninput: function(value){
+				Node.defaultHue = value;
+			}
+		}));
+		page.addComponent("init", new ComponentSlider({
+			bg: "initial",
+			label: "初始数量:",
+			options: [0, 0.16, 0.33, 0.50, 0.66, 0.83, 1],
+			//options: [0, 1/6, 2/6, 3/6, 4/6, 5/6, 1],
+			oninput: function(value){
+				Node.defaultValue = value;
+			}
+		}));
+		page.addComponent("radius", new ComponentSlider({
+			bg: "size",
+			label: "节点大小:",
+			options: [30, 45, 60, 75, 90, 105, 120],
+			oninput: function(value){
+				Node.DEFAULT_RADIUS = value;
+			}
+		}));
+		page.onedit = function(){
+
+			// Set color of Slider
+			var node = page.target;
+			var color = Node.COLORS[node.hue];
+			page.getComponent("init").setBGColor(color);
+			page.getComponent("radius").setBGColor(color);
+
+			// Focus on the name field IF IT'S "" or "?"
+			var name = node.label;
+			if(name=="" || name=="?") page.getComponent("label").select();
+
+		};
+		page.addComponent(new ComponentButton({
+			label: "删除节点",
+			//label: "delete circle",
+			onclick: function(node){
+				node.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Node", page);
+	})();
+
+	// Edge!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "返回顶部",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("strength", new ComponentSlider({
+			bg: "strength",
+			label: "<br><br>关系:",
+			//label: "Relationship:",
+			options: [1, -1],
+			oninput: function(value){
+				Edge.defaultStrength = value;
+			}
+		}));
+		page.addComponent("isDelayed", new ComponentSlider({
+			bg: "delay",
+			label: "<br><br>延迟关系:",
+			options: [false, true],
+			oninput: function(value){
+				// oninput callback if needed
+			}
+		}));
+		page.addComponent(new ComponentHTML({
+			html: "(要建立更强的关系，请绘制多个箭头！)<br><br>"+
+			"(要建立延迟关系，请绘制更长的箭头)"
+		}));
+		page.addComponent(new ComponentButton({
+			//label: "delete edge",
+			label: "删除箭头",
+			//label: "delete relationship",
+			onclick: function(edge){
+				edge.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Edge", page);
+	})();
+
+	// Label!
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentButton({
+			header: true,
+			label: "返回顶部",
+			onclick: function(){
+				self.showPage("Edit");
+			}
+		}));
+		page.addComponent("text", new ComponentInput({
+			label: "<br><br>标签:",
+			//label: "Label:",
+			textarea: true
+		}));
+		page.onshow = function(){
+			// Focus on the text field
+			page.getComponent("text").select();
+		};
+		page.onhide = function(){
+			
+			// If you'd just edited it...
+			var label = page.target;
+			if(!page.target) return;
+
+			// If text is "" or all spaces, DELETE.
+			var text = label.text;
+			if(/^\s*$/.test(text)){
+				// that was all whitespace, KILL.
+				page.target = null;
+				label.kill();
+			}
+
+		};
+		page.addComponent(new ComponentButton({
+			label: "删除标签",
+			onclick: function(label){
+				label.kill();
+				self.showPage("Edit");
+			}
+		}));
+		self.addPage("Label", page);
+	})();
+
+	// Edit
+	(function(){
+		var page = new SidebarPage();
+		page.addComponent(new ComponentHTML({
+			html: ""+
+			
+			"<b style='font-size:1.4em'>LOOPY</b> (v1.1)<br>系统思维工具<br><br>"+
+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"examples\"])'>查看示例</span> "+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"howto\"])'>使用教程</span> "+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"credits\"])'>致谢</span><br><br>"+
+
+			"<hr/><br>"+
+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"save_link\"])'>保存为链接</span> <br><br>"+
+			"<span class='mini_button' onclick='publish(\"export/file\")'>保存为文件</span> "+
+			"<span class='mini_button' onclick='publish(\"import/file\")'>从文件加载</span> <br><br>"+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"embed\"])'>嵌入到您的网站</span> <br><br>"+
+			"<span class='mini_button' onclick='publish(\"modal\",[\"save_gif\"])'>使用 LICEcap 制作 GIF</span> <br><br>"+
+
+			"<hr/><br>"+
+				
+			"<a target='_blank' href='../'>LOOPY</a> 由 "+
+			"<a target='_blank' href='http://ncase.me'>nicky case</a> 制作，"+
+			"感谢您在 <a target='_blank' href='https://www.patreon.com/ncase'>patreon</a> 上的支持 &lt;3<br><br>"+
+			"<span style='font-size:0.85em'>附：推荐阅读 <a target='_blank' href='https://www.amazon.com/Thinking-Systems-Donella-H-Meadows/dp/1603580557'>《系统思考》</a></span>"
+
+		}));
+		self.addPage("Edit", page);
+	})();
+
+	// Ctrl-S to SAVE
+	subscribe("key/save",function(){
+		if(Key.control){ // Ctrl-S or ⌘-S
+			publish("modal",["save_link"]);
+		}
+	});
+
+}
+
+function SidebarPage(){
+
+	// TODO: be able to focus on next component with an "Enter".
+
+	var self = this;
+	self.target = null;
+
+	// DOM
+	self.dom = document.createElement("div");
+	self.show = function(){ self.dom.style.display="block"; self.onshow(); };
+	self.hide = function(){ self.dom.style.display="none"; self.onhide(); };
+
+	// Components
+	self.components = [];
+	self.componentsByID = {};
+	self.addComponent = function(propName, component){
+
+		// One or two args
+		if(!component){
+			component = propName;
+			propName = "";
+		}
+
+		component.page = self; // tie to self
+		component.propName = propName; // tie to propName
+		self.dom.appendChild(component.dom); // add to DOM
+
+		// remember component
+		self.components.push(component);
+		self.componentsByID[propName] = component;
+
+		// return!
+		return component;
+
+	};
+	self.getComponent = function(propName){
+		return self.componentsByID[propName];
+	};
+
+	// Edit
+	self.edit = function(object){
+
+		// New target to edit!
+		self.target = object;
+
+		// Show each property with its component
+		for(var i=0;i<self.components.length;i++){
+			self.components[i].show();
+		}
+
+		// Callback!
+		self.onedit();
+
+	};
+
+	// TO IMPLEMENT: callbacks
+	self.onedit = function(){};
+	self.onshow = function(){};
+	self.onhide = function(){};
+
+	// Start hiding!
+	self.hide();
+
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// COMPONENTS ///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+function Component(){
+	var self = this;
+	self.dom = null;
+	self.page = null;
+	self.propName = null;
+	self.show = function(){
+		// TO IMPLEMENT
+	};
+	self.getValue = function(){
+		return self.page.target[self.propName];
+	};
+	self.setValue = function(value){
+		
+		// Model's been changed!
+		publish("model/changed");
+
+		// Edit the value!
+		self.page.target[self.propName] = value;
+		self.page.onedit(); // callback!
+		
+	};
+}
+
+function ComponentInput(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: label + text input
+	self.dom = document.createElement("div");
+	var label = _createLabel(config.label);
+	var className = config.textarea ? "component_textarea" : "component_input";
+	var input = _createInput(className, config.textarea);
+	input.oninput = function(event){
+		self.setValue(input.value);
+	};
+	self.dom.appendChild(label);
+	self.dom.appendChild(input);
+
+	// Show
+	self.show = function(){
+		input.value = self.getValue();
+	};
+
+	// Select
+	self.select = function(){
+		setTimeout(function(){ input.select(); },10);
+	};
+
+}
+
+function ComponentSlider(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// TODO: control with + / -, alt keys??
+
+	// DOM: label + slider
+	self.dom = document.createElement("div");
+	var label = _createLabel(config.label);
+	self.dom.appendChild(label);
+	var sliderDOM = document.createElement("div");
+	sliderDOM.setAttribute("class","component_slider");
+	self.dom.appendChild(sliderDOM);
+
+	// Slider DOM: graphic + pointer
+	var slider = new Image();
+	slider.draggable = false;
+	slider.src = "css/sliders/"+config.bg+".png";
+	slider.setAttribute("class","component_slider_graphic");
+	// If image fails to load (e.g., shape.png doesn't exist), use a gray background
+	slider.onerror = function(){
+		slider.style.background = "#ddd";
+		slider.style.width = "250px";
+		slider.style.height = "50px";
+	};
+	var pointer = new Image();
+	pointer.draggable = false;
+	pointer.src = "css/sliders/slider_pointer.png";
+	pointer.setAttribute("class","component_slider_pointer");
+	sliderDOM.appendChild(slider);
+	sliderDOM.appendChild(pointer);
+	var movePointer = function(){
+		var value = self.getValue();
+		var optionIndex = config.options.indexOf(value);
+		var x = (optionIndex+0.5) * (250/config.options.length);
+		pointer.style.left = (x-7.5)+"px";
+	};
+
+	// On click... (or on drag)
+	var isDragging = false;
+	var onmousedown = function(event){
+		isDragging = true;
+		sliderInput(event);
+	};
+	var onmouseup = function(){
+		isDragging = false;
+	};
+	var onmousemove = function(event){
+		if(isDragging) sliderInput(event);
+	};
+	var sliderInput = function(event){
+
+		// What's the option?
+		var index = event.x/250;
+		var optionIndex = Math.floor(index*config.options.length);
+		var option = config.options[optionIndex];
+		if(option===undefined) return;
+		self.setValue(option);
+
+		// Callback! (if any)
+		if(config.oninput){
+			config.oninput(option);
+		}
+
+		// Move pointer there.
+		movePointer();
+
+	};
+	_addMouseEvents(slider, onmousedown, onmousemove, onmouseup);
+
+	// Show
+	self.show = function(){
+		movePointer();
+	};
+
+	// BG Color!
+	self.setBGColor = function(color){
+		slider.style.background = color;
+	};
+
+}
+
+function ComponentButton(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: just a button
+	self.dom = document.createElement("div");
+	var button = _createButton(config.label, function(){
+		config.onclick(self.page.target);
+	});
+	self.dom.appendChild(button);
+
+	// Unless it's a HEADER button!
+	if(config.header){
+		button.setAttribute("header","yes");
+	}
+
+}
+
+function ComponentHTML(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// just a div
+	self.dom = document.createElement("div");
+	self.dom.innerHTML = config.html;
+
+}
+
+function ComponentOutput(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: just a readonly input that selects all when clicked
+	self.dom = _createInput("component_output");
+	self.dom.setAttribute("readonly", "true");
+	self.dom.onclick = function(){
+		self.dom.select();
+	};
+
+	// Output the string!
+	self.output = function(string){
+		self.dom.value = string;
+	};
+
+}
+
+function ComponentToggle(config){
+
+	// Inherit
+	var self = this;
+	Component.apply(self);
+
+	// DOM: label + checkbox
+	self.dom = document.createElement("div");
+	var label = _createLabel(config.label);
+	self.dom.appendChild(label);
+	
+	var checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	checkbox.setAttribute("class", "component_toggle");
+	checkbox.onchange = function(event){
+		self.setValue(checkbox.checked);
+	};
+	self.dom.appendChild(checkbox);
+
+	// Show
+	self.show = function(){
+		checkbox.checked = self.getValue();
+	};
+
+}
